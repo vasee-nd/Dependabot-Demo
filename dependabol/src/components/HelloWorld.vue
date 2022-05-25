8c883aeb62bd091befe26b203655f960b3e4883a
vasee-nd <vasee@navadhiti.com>
2022-05-25 13:23:58 +0530

    test

--- dependabol/src/components/HelloWorld.vue
+++ dependabol/src/components/HelloWorld.vue
@@ -12,7 +12,6 @@
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
     <h3>Essential Links</h3>
-    <Profile />
     <ul>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
@@ -29,16 +28,11 @@
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
   </div>
-  
 </template>
 
 <script>
-import Profile from "./Profile.vue"
 export default {
   name: 'HelloWorld',
-  components: {
-    Profile
-  },
   props: {
     msg: String
   }
--- dependabol/src/components/Profile.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>Profile page</h1>
-</template>
